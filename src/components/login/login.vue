fb45d26a9bfbccab5c1fbbac499d106e959ef592
HuaMao <huamao14@gmail.com>
2019-05-20 20:42:04 +0700

    login component

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div>
+    <h1>登陆组件</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
 
 <style>
-  #app {
-    height: 100%;
-  }
+
 </style>
